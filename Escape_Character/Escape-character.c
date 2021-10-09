a047898317af64219eeba8260fd11d05f50b13f7
Urtaome <admin@urtaome.com>
2021-10-09 21:23:12 +0800

    Test 2021/10/9

---
+++ Escape_Character/Escape-character.c
@@ -0,0 +1,117 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+{
+	int a = 0;
+	printf("%d\n", ~a);
+	return 0;
+}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", sizeof(arr[0]));
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	printf("%d\n", sz);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	printf("%d\n", !a);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 10;
+//	printf("%d\n", !a);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 2;
+//	int b = a << 1;
+//	printf("%d\n", b);
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		printf("%d", arr[i]);
+//		i++;
+//	}
+//	char ch[5] = { 'a','b','c' };
+//	return 0;
+//}
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d%d", &num1, &num2);
+//	//int sum = num1 + num2;
+//	int sum = Add(num1, num2);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+//int main()
+//{
+//	int line = 0;
+//	while (line < 30000)
+//	{
+//		printf("coding:%d\n", line);
+//		line++;
+//	}
+//	if (line == 30000)
+//	{
+//		printf("Congrats! good offer");
+//	}
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int input = 0;
+//	printf("要好好学习吗？(1/0)");
+//	scanf("%d", &input);
+//
+//	if (input == 1)
+//		printf("拿到好offer\n");
+//	else
+//		printf("回家种田\n");
+//	return 0;
+//}
+//int main()
+//{
+//	//int a = 0;
+//	/*
+//	int b = 0;
+//	*/
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%d\n", strlen("c:\test\328\test.c"));
+//	return 0;
+//}
+//int main()
+//{
+//	printf("(are you ok??)");
+//	return 0;
+//}
+//int main()
+//{
+//	printf("c:\test\test.c");
+//	return 0;
+//}
