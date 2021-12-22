7ef0826e69e7c40ec84b478ca28ecdfe9ece2b68
Urtaome <admin@urtaome.com>
2021-12-22 17:17:55 +0800

    Test 2021/12/22

---
+++ Function/Function2/Function2/Source2.c
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int binary_search(int arr[], int k, int s)
+//{
+//	int left = 0;
+//	int right = s - 1;
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (arr[mid] > k)
+//		{
+//			right = mid - 1;
+//		}
+//		else if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//		}
+//		else
+//		{
+//			return mid;
+//		}
+//	}
+//	return -1;
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int key = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int ret = binary_search(arr, key, sz);
+//	if (ret == -1)
+//	{
+//		printf("找不到\n");
+//	}
+//	else
+//	{
+//		printf("找到了，下标是：%d\n", ret);
+//	}
+//	return 0;
+//}
+
+//void new_line()
+//{
+//	printf("hehe\n");
+//}
+//void three_line()
+//{
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		new_line();
+//	}
+//}
+#include "add.h"
+#pragma comment (lib, "add.lib")
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int c = Add(a, b);
+	printf("%d\n", c);
+	return 0;
+}
