02ba2a1a000a259c3c0917db72ad4b48e4090d52
Urtaome <admin@urtaome.com>
2021-12-22 17:13:46 +0800

    Test 2021/12/22

---
+++ Function2/Function2/Source1.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int is_prime(int n)
+//{
+//	int j = 0;
+//	for (j = 2; j < n; j++)
+//	{
+//		if (n % j == 0)
+//		{
+//			return 0;
+//		}
+//	}
+//	return 1;
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		if (is_prime(i) == 1)
+//		{
+//			count++;
+//			printf("%d ", i);
+//		}
+//	}
+//	printf("\nCount=%d", count);
+//	return 0;
+//}
+
+//int is_leap_year(int x)
+//{
+//	if ((x % 4 == 0) && (x % 100 != 0) || (x % 400 == 0))
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return 0;
+//	}
+//}
+//int main()
+//{
+//	int year = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		if (is_leap_year(year) == 1)
+//		{
+//			printf("%d ", year);
+//		}
+//	}
+//	return 0;
+//}
