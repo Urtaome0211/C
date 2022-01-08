8c85735a36a22945b0ef05dd584260d0b936a61c
Urtaome <admin@urtaome.com>
2022-01-08 22:21:50 +0800

    Test 2022-1-8

---
+++ Array/Array/Source.c
@@ -0,0 +1,14 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//
+//int main()
+//{
+//    int arr[10] = { 0 };
+//    int i = 0;
+//    for (i = 0; i < 10; i++)
+//    {
+//        printf("&arr[%d} = %p\n", i, &arr[i]);
+//        //%p-按地址的格式打印，为十六进制
+//    }
+//    return 0;
+//}
